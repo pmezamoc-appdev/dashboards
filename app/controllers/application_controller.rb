128b5ee19ed0b83878cf89c3c6ce042cbe930241
pmezamoc <pmezamoc@chicagobooth.edu>
2023-01-29 23:16:41 +0000

    First try

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  def launch
+    render({:template => "/exchange/main.html.erb"})
+  end 
+
+  def list
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+    @parse_data = JSON.parse(@raw_data)
+    @symbols_hash = @parse_data.fetch("symbols").keys
+
+    render({:template => "/exchange/forex.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get("/", {:controller => "application", :action => "launch"})
+  get("/rates", {:controller => "application", :action => "list"})
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
