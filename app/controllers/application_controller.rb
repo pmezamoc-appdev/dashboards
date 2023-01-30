760c2b473c71d3caec235d4d1ab93c237741e145
pmezamoc <pmezamoc@chicagobooth.edu>
2023-01-30 00:21:19 +0000

    Finalize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,21 @@ class ApplicationController < ActionController::Base
 
     render({:template => "/exchange/forex.html.erb"})
   end
+
+  def convert
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+    @parse_data = JSON.parse(@raw_data)
+    @symbols_hash = @parse_data.fetch("symbols").keys
+
+    @initial_currency = params.fetch("symbol")
+    render({:template => "/exchange/step_two.html.erb"})
+  end
+
+  def values
+    @initial_currency = params.fetch("symbol_one")
+    @ending_currency = params.fetch("symbol_two")
+    @raw_data = open("https://api.exchangerate.host/convert?from=#{@initial_currency}&to=#{@ending_currency}").read
+    @conversion = JSON.parse(@raw_data).fetch("info").fetch("rate")
+    render({:template => "/exchange/step_three.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get("/", {:controller => "application", :action => "launch"})
-  get("/rates", {:controller => "application", :action => "list"})
+  get("/forex", {:controller => "application", :action => "list"})
+  get("/forex/:symbol", {:controller => "application", :action => "convert"})
+  get("/forex/:symbol_one/:symbol_two", {:controller => "application", :action => "values"})
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
